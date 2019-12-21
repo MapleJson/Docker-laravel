8b0540005b068e5cb363042a359774f9b9b1ef13
maple <maple@git.com>
2019-12-21 19:33:31 +0800

    docker-build

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # 定义镜像版本
-read -p "请输入你的镜像版本号[1.0]:" version
+read -p "请输入你的镜像版本号,默认为[1.0]:" version
 
 if [[ -z "${version}" ]]; then
 	version=1.0
 fi
 # PHP版本
-read -p "请输入你要编译的PHP版本,以空格分隔[56 70 71 72]:" phpVersion
+read -p "请输入你要编译的PHP版本,以空格分隔,默认为[56 70 71 72]:" phpVersion
 
 if [[ -z "${phpVersion}" ]]; then
 	phpVersion="56 70 71 72"
@@ -22,6 +22,7 @@ build_docker()
 {
     for ver in ${phpVersion}
     do
+        echo "=======> 开始生成镜像 ${hubName}/laravel-php${ver}:${version} <======="
         cd ${dockerFilePath}${ver}
         # 编译docker镜像
         docker build -t ${1}/laravel-php${ver}:${version} .
@@ -31,19 +32,27 @@ build_docker()
         else
             sed -i "s#image: maple52zoe/laravel-php${ver}:.\.0#image: ${1}/laravel-php${ver}:${version}#g" ${ymlFile}
         fi
-
+        echo "=======> 镜像${hubName}/laravel-php${ver}:${version}生成完成! <======="
+        if [[ ${2} == y ]]; then
+            # push到自己的仓库
+            echo -e "=======> 开始push镜像 ${hubName}/laravel-php${ver}:${version} <======="
+            docker push ${hubName}/laravel-php${ver}:${version}
+            echo -e "=======> 镜像 ${hubName}/laravel-php${ver}:${version} push完成! <======="
+        fi
     done
 }
 
-read -p "请输入你的仓库名称,不输入默认为[maple52zoe]:" hubName
+read -p "请输入你的仓库名称,默认为[maple52zoe]:" hubName
 
 if [[ -z "${hubName}" ]]; then
 	hubName=maple52zoe
 fi
 
-echo ${hubName}
+read -p "是否登录docker,并push镜像至你的docker hub？请输入[y|n],默认为[n]:" num
 
-read -p "是否登录docker,并push镜像至你的docker hub？确认请输入y:" num
+if [[ -z "${num}" ]]; then
+	num=n
+fi
 
 if [[ ${num} == y ]]; then
     # 登录docker
@@ -51,10 +60,5 @@ if [[ ${num} == y ]]; then
 fi
 
 # 编译docker
-build_docker ${hubName}
-
-if [[ ${num} == y ]]; then
-    # push到自己的仓库
-    docker push ${hubName}/laravel-php${ver}:${version}
-fi
+build_docker ${hubName} ${num}
 
